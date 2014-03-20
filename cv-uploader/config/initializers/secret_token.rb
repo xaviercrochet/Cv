f293a83fbf805540021a376a0657ec468a84b738
Xavier Crochet <xacrochet@gmail.com>
2014-03-20 11:19:15 +0100

    initial commit

---
+++ cv-uploader/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CvUploader::Application.config.secret_key_base = 'e4a61b5c6928ff5c589f53ae58f636ffe67d9b315951b63dff72f4bf516c533b6a3c4175d4f2e98a12ebef13d069824d8581edaf79b0f80b5eaa3eb1afdf80c7'
---
+++ cv-uploader/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CvUploader::Application.config.session_store :cookie_store, key: '_cv-uploader_session'
