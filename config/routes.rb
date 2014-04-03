a12b21aa00e8ae362911bf50afdd1bfc096bfe09
Xavier Crochet <xacrochet@gmail.com>
2014-04-03 15:19:01 +0200

    Added devise gem & basic config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 CvUploader::Application.routes.draw do
+  devise_for :admins, :skip => [:registrations]
   root "users#home"
   match '', to: 'user#home', via: 'get'
   resources :users
+  
+
   
 end
